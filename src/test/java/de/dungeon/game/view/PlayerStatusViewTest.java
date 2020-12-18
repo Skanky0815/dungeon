eb2c5c8802716a34d47c46cee46dccaf3fa0e9b3
Skanky0815 <rico-schulz@web.de>
2020-12-18 22:19:06 +0100

    remove unused

--- src/test/java/de/dungeon/game/view/PlayerStatusViewTest.java
+++ src/test/java/de/dungeon/game/view/PlayerStatusViewTest.java
@@ -8,8 +8,6 @@ import de.dungeon.game.character.property.Range;
 
 import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PlayerStatusViewTest extends ViewTestCase {
