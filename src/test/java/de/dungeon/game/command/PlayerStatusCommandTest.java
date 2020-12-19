a2248bc139bfd813c5b2201d45cf8d10ffdf4bf1
Skanky0815 <rico-schulz@web.de>
2020-12-19 19:32:01 +0100

    fix code style

--- src/test/java/de/dungeon/game/command/PlayerStatusCommandTest.java
+++ src/test/java/de/dungeon/game/command/PlayerStatusCommandTest.java
@@ -10,7 +10,7 @@ public class PlayerStatusCommandTest {
 
     @Test
     void doActionShouldCallTheRenderMethodOfTheView() {
-        final var player = (new PlayerBuilder()).build("player", 1,1,1, 1).get();
+        final var player = (new PlayerBuilder()).build("player", 1, 1, 1, 1).get();
         final var viewMock = mock(PlayerStatusView.class);
 
         final var command = new PlayerStatusCommand(viewMock);
