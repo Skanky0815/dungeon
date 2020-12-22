26b3a38221dd0a6b61070f171b56d55fbee03af6
Skanky0815 <rico-schulz@web.de>
2020-12-23 00:14:33 +0100

    fix test for damage handler

--- src/test/java/de/dungeon/game/battle/DamageHandlerTest.java
+++ src/test/java/de/dungeon/game/battle/DamageHandlerTest.java
@@ -3,13 +3,14 @@ package de.dungeon.game.battle;
 import de.dungeon.game.character.Character;
 import de.dungeon.game.character.property.Dodge;
 import de.dungeon.game.rule.TestResult;
+import de.dungeon.game.view.ViewTestCase;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-class DamageHandlerTest {
+class DamageHandlerTest extends ViewTestCase {
 
     @Test
     void makeDamageShouldMake8DamageAtTheDefender() {
@@ -34,12 +35,7 @@ class DamageHandlerTest {
     @Test
     void makeDamageShouldMake3DamageAtTheDefender() {
         var dodgeMock = mock(Dodge.class);
-        when(dodgeMock.test()).thenReturn(new TestResult(0, 0) {
-            @Override
-            public boolean isSuccess() {
-                return true;
-            }
-        });
+        when(dodgeMock.isTheTestSuccessfully()).thenReturn(true);
 
         var attackerStub = new Character("char A", 20, 2, new Dodge(5));
         var defenderStub = new Character("char B", 20, 2, dodgeMock);
